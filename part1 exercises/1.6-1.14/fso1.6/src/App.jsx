4bd9ce1069d04b92f519bfd5b2948c4577009224
giorgivashakmadze <vashakmadze91@gmail.com>
2024-02-29 02:31:05 +0400

    1.9

--- part1 exercises/1.6-1.14/fso1.6/src/App.jsx
+++ part1 exercises/1.6-1.14/fso1.6/src/App.jsx
@@ -1,37 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const totalFB = good + neutral + bad;
-  const averageScore = totalFB === 0 ? 0 : (good - bad) / totalFB;
-  // Calculate percentage of positive feedback
-  const positiveFeedbackPercentage = totalFB === 0 ? 0 : (good / totalFB) * 100;
+const Statistics = ({ good, neutral, bad }) => {
+  const totalFeedback = good + neutral + bad;
+  const averageScore = totalFeedback === 0 ? 0 : (good - bad) / totalFeedback;
+  const positiveFeedbackPercentage =
+    totalFeedback === 0 ? 0 : (good / totalFeedback) * 100;
 
   return (
     <div>
-      <h1>Feedback App</h1>
+      <h2>Feedback Statistics:</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Average Score: {averageScore.toFixed(2)}</p>
+      <p>Positive Feedback Percentage: {positiveFeedbackPercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const totalFeedback = good + neutral + bad;
+
+  return (
+    <div>
+      <h1>Unicafe Feedback App</h1>
 
       <div>
+        <h2>Click buttons to give feedback:</h2>
         <button onClick={() => setGood(good + 1)}>Good</button>
         <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
         <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
 
-      <div>
-        <h2>Feedback Statistics:</h2>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total Feedback: {totalFB}</p>
-        <p>Average Score: {averageScore.toFixed(2)}</p>
-        <p>
-          Positive Feedback Percentage: {positiveFeedbackPercentage.toFixed(2)}%
-        </p>
-      </div>
+      {totalFeedback > 0 && <Statistics good={good} neutral={neutral} bad={bad} />}
     </div>
   );
 };
