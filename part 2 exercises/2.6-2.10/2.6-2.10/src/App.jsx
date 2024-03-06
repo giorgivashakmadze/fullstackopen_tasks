a8297239b3cc51de51f6ad83e74224c8a4ae2df8
giorgivashakmadze <vashakmadze91@gmail.com>
2024-03-06 13:07:33 +0400

    2.8 and 2.9

--- part 2 exercises/2.6-2.10/2.6-2.10/src/App.jsx
+++ part 2 exercises/2.6-2.10/2.6-2.10/src/App.jsx
@@ -1,32 +1,50 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     // Check if the new name already exists in the phonebook
-    if (persons.some(person => person.name === newName)) {
+    if (newName === "") {
+      alert("Name cannot be empty");
+      return; // Exit the function to prevent further execution
+    }
+
+    if (persons.some((person) => person.name === newName)) {
       // Issue a warning if the name already exists
       alert(`${newName} is already added to the phonebook`);
     } else {
       // Create a new person object with the entered name
-      const newPerson = { name: newName };
+      const newPerson = { name: newName, number: newNumber };
 
       // Update the persons state with the new person
       setPersons([...persons, newPerson]);
 
-      // Clear the input field
+      // Clear the input fields
       setNewName("");
+      setNewNumber("");
     }
   };
 
+  // Filter the persons based on the search term
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      
       <form onSubmit={handleSubmit}>
         <div>
           name:{" "}
@@ -37,13 +55,32 @@ const App = () => {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            type="number"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+
+      <h2>Search</h2>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
       <h2>Numbers</h2>
       <ul>
-      {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>
+            {person.name}: {person.number}
+          </li>
         ))}
       </ul>
     </div>
